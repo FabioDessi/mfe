dc3a8ce517091ef4de9b87ba85ad39ed1046f8cb
Fabio <f.dessi@fabiodessi.dev>
2021-07-23 01:40:34 +0200

    feat(container): changed shown message again

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>I'm the Container, Hi There!</h1>
+    <h1>Container, Hi There!</h1>
     <hr />
     <MarketingApp />
   </div>;
